974658a70fa60be13403c32d8c563651ed4bc63f
Marcos Quaresma <quaresmamarcoslol@gmail.com>
2026-09-17 05:40:02 -0300

    demonstração_de_dados

---
+++ 02_dados.py
@@ -0,0 +1,17 @@
+# tipos de dados em Pyhton
+# Catalogo de filmes
+
+nome = "Top gun Maverick"
+lancamento = 2023
+nota_do_filme = 9.5
+inlcuso_no_plano = False
+
+print(nome)
+print(lancamento)
+print(nota_do_filme)
+print(inlcuso_no_plano)
+
+print(type(nome))
+print(type(lancamento))
+print(type(nota_do_filme))
+print(type(inlcuso_no_plano))
