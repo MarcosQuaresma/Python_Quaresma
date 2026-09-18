c09a1f4cd971f1e78542ab7d083f7616b731ec9f
Marcos Quaresma <quaresmamarcoslol@gmail.com>
2026-09-18 02:40:37 -0300

    manipulacao_de_listas

---
+++ aulas/09_lista.py
@@ -0,0 +1,8 @@
+filme1 = ["O Mascara", 1994, 7.0, True]
+
+lista_filmes = ["O Poderoso Chefão", "Vingadores - Ultimato", "A Origem",
+                 "Batman O Cavaleiro Das Trevas", "Ela"]
+
+# Buscar os dois primeros itens na lsita
+print(lista_filmes[-1:]) # vejamos, eu decidi invocar em uma lista os dois primeiros indices, na lista os indices são separados por ","
+                        # os dois primeiros indices, na lista os indices são separados por ","
