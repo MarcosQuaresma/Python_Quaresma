c067d98979e07dd3b6383d226d7ef2ee5c493786
Marcos Quaresma <quaresmamarcoslol@gmail.com>
2026-09-18 04:27:05 -0300

    Tuplas

---
+++ aulas/11_tuplas.py
@@ -0,0 +1,17 @@
+lista_filmes = ("O Poderoso Chefão", "Vingadores - Ultimato", "A Origem",
+                 "Batman O Cavaleiro Das Trevas", "Ela")
+
+# 1 Buscar os dois primeiros items
+print(lista_filmes[:2])
+
+# 2 Busca o ultimo item da tupla
+print(lista_filmes[-1])
+
+# 3 Buscar o filme até uma determinadapsição
+print(lista_filmes[:3])
+
+# 4 Buscar filmes de uma posição em diante 
+print(lista_filmes[3:])
+
+# 5 Recuperar um item da tupla pelo nome
+print(lista_filmes.index("Ela"))
